882174f8b26749fe50987894b3ab4b5ec14e5814
Vasyl Prokudin <voprokudin@gmail.com>
2023-03-06 10:43:20 +0200

    clean up

--- app/src/main/java/com/jotit/mvc_evaluation_app/screens/drawing/DrawingViewMvcImpl.java
+++ app/src/main/java/com/jotit/mvc_evaluation_app/screens/drawing/DrawingViewMvcImpl.java
@@ -1,7 +1,6 @@
 package com.jotit.mvc_evaluation_app.screens.drawing;
 
 import android.graphics.Bitmap;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +8,6 @@ import android.view.ViewTreeObserver;
 import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.material.button.MaterialButton;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
@@ -29,7 +26,6 @@ public class DrawingViewMvcImpl extends DrawingViewMvc {
         MaterialButton btnUndo = findViewById(R.id.btnUndo);
         MaterialButton btnClear = findViewById(R.id.btnClear);
         MaterialButton btnRecognizeMlKit = findViewById(R.id.btnRecognizeMlKit);
-        MaterialButton btnRecognizeCloudVision = findViewById(R.id.btnRecognizeCloudVision);
         btnUndo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,27 +63,6 @@ public class DrawingViewMvcImpl extends DrawingViewMvc {
                         }
                     }
                 });
-            }
-        });
-        btnRecognizeCloudVision.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bitmap bmp = paint.save();
-                com.google.android.gms.vision.text.TextRecognizer textRecognizer = new com.google.android.gms.vision.text.TextRecognizer.Builder(view.getContext()).build();
-                Frame frameImage = new Frame.Builder().setBitmap(bmp).build();
-                SparseArray<TextBlock> textBlockArray = textRecognizer.detect(frameImage);
-                StringBuilder stringImageText = new StringBuilder();
-                for (int i = 0; i < textBlockArray.size(); i ++) {
-                    TextBlock textBlock = textBlockArray.get(textBlockArray.keyAt(i));
-                    stringImageText.append(" ").append(textBlock.getValue());
-                }
-                for (DrawingViewMvc.Listener listener : getListeners()) {
-                    if (stringImageText.length() == 0) {
-                        listener.onTextRecognitionError();
-                    } else {
-                        listener.onTextRecognized(stringImageText.toString());
-                    }
-                }
             }
         });
         paint.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
